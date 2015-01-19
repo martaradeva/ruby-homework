f89743f3c126a3c0cb1bdd4af6ce89136e9748eb
Martina Radeva <martaradeva@yahoo.com>
2015-01-19 15:30:21 +0200

    Working horizontal and vertical group

--- tasks/04/solution-m.rb
+++ tasks/04/solution-m.rb
@@ -2,53 +2,83 @@ module UI
 
   class TextScreen
 
+    # def self.draw &block
+    #   compose &block
+    #   puts "screen to render = #{@screen.group.inspect}"
+    #   @screen.render
+    # end
+
+    # def self.compose &block
+    #   @screen = Screen.new
+    #   @screen.instance_eval(&block)
+    #   #puts "screen.group in compose = #{@screen.group.inspect}"
+    # end
+    # def initialize
+    #   @screen = Screen.new
+    # end
+
     def self.draw &block
-      screen = Screen.new
-      screen.instance_eval(&block)
-      puts screen.render.inspect
-      screen.render
+      @screen = Screen.new
+      @screen.instance_eval(&block)
+      puts "screen.group in render = #{@screen.group.inspect}"
+      @screen.group.flatten.join
     end
+
+
   end
 
   class Screen
+    attr_accessor :group
+
     def initialize()
-      @screen = []
+      # @screen = []
+      @group = []
     end
 
     def add_group &block
-      @screen << [instance_eval(&block)]
+      @group << instance_eval(&block)
+      #
       #@screen << element
       # add instance_eval so all added elements are executed beforehand?
     end
 
     def label (text, style: nil, border: nil)
       @lbl = TextLabel.new(text)
-      #puts @lbl
+      #puts @lbl.label
       add_group {@lbl}
+      puts "group when label = #{@group.inspect}"
+      @group
       #puts @screen.inspect
     end
 
     def vertical &block
-      instance_eval(&block)
-      @screen.map{|element| element << "\n" }
-      #puts "*"
-      puts @screen.inspect
+      @partial = UI::Screen.compose &block
+      puts "partial of vertical = #{@partial.inspect}"
+      @group = @partial.map{|element| [ element, "\n" ] }
+      puts "group after vertical = #{@group.inspect}"
+      @group
     end
 
     def horizontal &block
-      #@screen << [ yield ]
-      instance_eval(&block)
-      @screen = [ @screen]
-      puts @screen.inspect
+      @partial = UI::Screen.compose &block
+      puts "partial of horizontal = #{@partial.inspect}"
+      @partial << "\n"
+      @group << @partial
+      puts "group after horizontal = #{@group.inspect}"
+      @group
     end
 
-    # def newline
-    #   @screen << ["debug2"]
+    def self.compose &block
+      @screen = Screen.new
+      @screen.instance_eval(&block)
+      #puts "screen.group in compose = #{@screen.group.inspect}"
+    end
+
+    # def render
+    #   puts "group.inspect = #{@group.inspect}"
+    #   puts "group.flatten = #{@group.flatten.inspect}"
+    #   @group.flatten.join
     # end
-
-    def render
-      @screen.flatten.join
-    end
   end
 
   class TextLabel
@@ -63,13 +93,9 @@ module UI
     end
 
     def to_s
+      # puts @label.to_s
       @label.to_s
     end
-
-    # def newline
-    #   @label = @label + "debug1"
-    #   #returns screen hash with "\n"
-    # end
 
   end
 end
